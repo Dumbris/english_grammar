f214c5ba7773217938c2bb7127bbc447b3d96721
Algis Dumbris <a.dumbris@gmail.com>
2018-03-28 09:53:07 +0300

    Added talking about future

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@
 ## Docs here https://github.com/asciidoctor/docker-asciidoctor
 
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -r asciidoctor-diagram *.adoc
+docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram *.adoc
 #asciidoctor -r asciidoctor-diagram *.adoc
